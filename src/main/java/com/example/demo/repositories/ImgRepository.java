12b682bd618a196c0c05b922b84705855ffcd24d
HeavyGunner17 <heavygunneralv17@gmail.com>
2022-08-19 23:10:32 -0300

    asdasdasd

---
+++ src/main/java/com/example/demo/repositories/ImgRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repositories;
+
+// import java.util.ArrayList;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.ImgModel;
+
+@Repository
+public interface ImgRepository extends CrudRepository<ImgModel, Long> {
+   
+    
+}
